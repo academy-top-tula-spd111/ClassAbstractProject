7ac8bb9dfd3481b54878c6ee537813a33cf0abf1
GitStudent2022 <torelgame@mail.ru>
2022-09-17 17:46:46 +0300

    Добавьте файлы проекта.

---
+++ ClassAbstractProject/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ClassAbstractProject
+{
+    class Person<T>
+    {
+        public T Id { get; set; }
+        public string Name { get; set; }
+
+        public void Method(T variable)
+        {
+
+        }
+    }
+
+    class User<T> : Person<T>
+    {
+
+    }
+
+
+    abstract class Animal
+    {
+        public abstract string Voice();
+
+        public void Method()
+        {
+            Console.WriteLine("Non abstract");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override string Voice()
+        {
+            return "Myau";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.Method();
+
+            Animal animal = new Cat();
+             
+
+            Person<int> person = new Person<int>();
+            Person<string> person2 = new Person<string>();
+        }
+    }
+}
